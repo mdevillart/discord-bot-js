caa99597fa894f1c8f8b59d01dbdcd4199ea8e37
mdevillart <mdevillart@outlook.com>
2020-05-02 16:11:13 -0300

    comments added to the !kick feature

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
+// import discordjs and configjson
 const Discord = require('discord.js');
 const { prefix, token, ban_phrases_array, giphyToken } = require('./config.json');
 const client = new Discord.Client();
-
+// import giphy api
 var GphApiClient = require('giphy-js-sdk-core');
 giphy = GphApiClient(giphyToken);
-
+// start bot
 client.once('ready', () => {
     console.log('Ready!')
 })
-
-function getRandomBanPhrase() {
-    return ban_phrases_array[~~(Math.random()*ban_phrases_array.length)];
-}
-
+// !kick feature
 client.on('message', message => {
+    function getRandomBanPhrase() {
+        return ban_phrases_array[~~(Math.random()*ban_phrases_array.length)];
+    }    
 
     function hasPermission(member) {
         return message.member.hasPermission(['KICK_MEMBERS', 'BAN_MEMBERS']);
@@ -23,31 +23,33 @@ client.on('message', message => {
         return message.content.startsWith(`${prefix}kick`);
     }
 
-
     if (hasPermission() && commandPrefix()) { 
 
         let member = message.mentions.members.first();
         member.kick().then((member) => {           
 
-         giphy.search('gifs', {"q": "loki approves" })
+         giphy.search('gifs', {"q": "failed" })
             .then((response) => {
                 var totalResponses = response.data.length;
                 var responseIndex = Math.floor((Math.random() * 10) + 1) % totalResponses;
                 var responseFinal = response.data[responseIndex];
                 
                 message.channel
-                    .send(`${getRandomBanPhrase()} \n ${member.displayName} foi kickado! :skull:`, {
+                    .send(`${getRandomBanPhrase()} \n ${member.displayName} was kicked! :skull:`, {
                     files: [responseFinal.images.fixed_height.url],
                     tts: true
                 })
-                                .catch(error => message.channel.send(error))
+            .catch(error => message.channel.send(error))
+            // catch error at gif search
             }) 
             .catch(() => {
                 message.channel.send("404!");
+            // catch a error at kicking a member
             }) 
         })
         .catch((error) => message.channel.send(`Cannot kick a member ${error}`))
+        // catch error at permission + prefix
     } 
 })
-
+// login importing token from configjson
 client.login(token);
